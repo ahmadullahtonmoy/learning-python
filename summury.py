fd1a7eefd03add6c234a4ce72400f0763e9f0d4e
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-10-28 17:13:03 +0600

    summury updated

--- summury.py
+++ summury.py
@@ -1,6 +1,10 @@
 # how to output a code in python?
 # how to take user input?
 # how to set input level?
-# 
+# how to declare a variable in Python?
+# there is no const in Python...
+# how to import parent file?
+# how to call and over_write variable from another file?
+# there are so many option for variable. variable name and then a single DOT...
 # 
 # 
