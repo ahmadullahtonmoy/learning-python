aebb79553a07a25350bfc008aa780a1db3ca960a
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-10-28 14:25:48 +0600

    more input added

--- input.py
+++ input.py
@@ -2,5 +2,9 @@
 input()
 
 # but its need a variable for input data contain
-user_name = input("Enter Your Name: ")
-company_name = input("Enter Company Name: ")
+user_name = input()
+company_name = input()
+
+# i can set a label in the input to know which type input which one
+user_mail = input("Enter User Email Here: ")
+user_number = input("Enter User number Here: ")
