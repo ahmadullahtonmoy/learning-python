03de3e676297b273fca0c9fa9dc18fe0e025633e
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-10-28 09:32:10 +0600

    all input code and label worked well

---
+++ input.py
@@ -0,0 +1,6 @@
+# this is how to code an input in python
+input()
+
+# but its need a variable for input data contain
+user_name = input("Enter Your Name: ")
+company_name = input("Enter Company Name: ")
