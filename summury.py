4f099cda3d629519a45b9b60e92a4cd692c93b88
Ahmadullah Tonmoy <ahmadullahtonmoy@gmail.com>
2022-11-08 22:43:30 +0600

    summary updated

--- summury.py
+++ summury.py
@@ -3,7 +3,7 @@
 # how to set input level?
 # how to declare a variable in Python?
 # there is no const in Python... but there is a naming convention which is followed by programmers...
-# how to import parent file?
+# how to import parent's child file?
 # how to call and over_write variable from another file?
 # there are so many option for variable. variable name and then a single DOT...
 # 
