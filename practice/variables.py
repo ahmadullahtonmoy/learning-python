1dfd14b816327db520a70f2ec97b62268f4fc282
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-24 10:46:39 +0600

    same value for multiple var in one line added

--- practice/variables.py
+++ practice/variables.py
@@ -8,10 +8,20 @@ userPhonNumber = +13492243
 print("User Name:",userName)
 print("User Phone Number:",userPhonNumber)
 
+
 # multiple value assign in multiple variables in one line
 
 apple_price , orange_price , bananaPrice = 50 , 40 , 30
 
 print("Apple Price:",apple_price)
 print("Orange Price:",orange_price)
+print("Banana Price:",bananaPrice)
+
+
+# one value assign to multiple variables in one line
+
+applePrice = orangePrice = bananaPrice = 40
+
+print("Apple Price:",applePrice)
+print("Orange Price:",orangePrice)
 print("Banana Price:",bananaPrice)
