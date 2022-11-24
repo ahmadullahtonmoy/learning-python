5af278f674288ddebdb0d21aee95d34060c433a9
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2022-11-24 10:36:13 +0600

    variables declared

---
+++ practice/variables.py
@@ -0,0 +1,12 @@
+# declaring a variable
+# variable name + equals sign + value (variableName = value)
+
+userName = "Cat 5.0"
+
+userPhonNumber = +13492243
+
+print("User Name:",userName)
+print("User Phone Number:",userPhonNumber)
+
+# multiple value assign in multiple variables in one line
+
