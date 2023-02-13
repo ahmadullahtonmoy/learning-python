d1fcf0e11ea734d05e5543284655c05ebfd56a34
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2023-02-13 22:37:02 +0600

    new practice added

---
+++ 0_intro_to_py/2_simple_math.py
@@ -0,0 +1,20 @@
+# doing simple mathematics with variables.
+_num1 = 5
+_num2 = 10
+_num3 = 15
+_num4 = 20
+
+_res0 = _num1 + _num3
+print("5 + 15 =" , _res0)
+
+_res1 = _num4 - _num3
+print("20 - 15 =" ,  _res1)
+
+_res2 = _num1 * _num2
+print("5 * 10 =" , _res2)
+
+_res3 = _num4 / _num1
+print("20 / 5 =" , _res3)
+
+_res4 = _num4 % _num3
+print("Remain of 20 after divided with 15 =" , _res4)
---
+++ 1_conditionals_py/0_if_state.py
@@ -0,0 +1,20 @@
+_apple_price = 10
+_banana_price = 15
+_orange_price = 20
+_lichi_price = 25
+
+_budget0_0 = 30
+_budget0_1 = 20
+_budget0_2 = 10
+
+if _apple_price <= 15:
+    print("Get Apple to Me.")
+    print("The Apple Price is less then Fifteen.")
+
+
+if _orange_price < _budget0_0:
+    print("Bring Banana and Orange.")
+
+
+if _lichi_price <= _budget0_0:
+    print("Bring Lichi to Me.")
