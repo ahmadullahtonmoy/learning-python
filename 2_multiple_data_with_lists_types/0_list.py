048982c27a5c876485a81db7fa9aacd75dc55493
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2023-02-14 23:26:45 +0600

    py list study going on

---
+++ 2_multiple_data_with_lists_types/0_list.py
@@ -0,0 +1,19 @@
+_cloth_box = []
+_text0 = "Cloth Box Status:"
+_text1 = "Cloth Box Status After Added An Item:"
+print(_text0 , _cloth_box)
+
+_cloth_box.append("Center Cloth")
+print(_text1 , _cloth_box)
+
+_cloth_box.append("Shirt")
+print(_text1 , _cloth_box)
+
+_cloth_box.append("Sweater")
+print(_text1 , _cloth_box)
+
+_cloth_box.append("Jacket")
+print(_text1 , _cloth_box)
+
+_cloth_box.append("Shaal")
+print(_text1 , _cloth_box)
