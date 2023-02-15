11b597332709eea4fcaf69979c3d1d75eb4b821c
ahmadullahtonmoy <ahmadullahtonmoy@gmail.com>
2023-02-15 23:38:01 +0600

    added some practice

---
+++ 0_intro_to_py/0_start.py
@@ -0,0 +1,22 @@
+# this is a comment
+# getting out_put throw print_out
+print("Hi Python! This is Tonmoy from SuperNova PC. I Love You!")
+
+# getting numeric output
+print("This is a Number" , 12)
+
+# getting simple mathematic output using print()
+print("2 + 2 =" , 2 + 2)
+print("4 - 2 =" , 4 - 2)
+print("2 * 1 =" , 2 * 1)
+print("4 / 2 =" , 4 / 2)
+
+# getting data types with type()
+print(type(2))
+
+# getting true or false
+print("5 is less then 2. It's true or False? The Ans Is:" , 5 < 2)
+print("""
+10 is Greater Then 5.
+It's True Or False?
+The Answer is:""" , 10 > 5)
---
+++ 0_intro_to_py/1_variables.py
@@ -0,0 +1,36 @@
+
+_apple_price = 10
+_orange_price = 15
+_lichi_price , _banana_price , _jackfruit_price = 20 , 25 , 30
+
+print("Apple Price:" , _apple_price)
+print("Lichi Price:" , _lichi_price)
+
+_type_0 = type(_apple_price)
+
+print("Type Of _apple_price Var:" , _type_0)
+
+_cat_name = "Tuku"
+_car_name = "Toyota"
+_user_first_name = "Tom"
+_user_last_name = "Hench"
+_user_full_name = _user_first_name + " " + _user_last_name
+
+print("Car Name:" , _car_name)
+print("User First Name:" , _user_first_name)
+print("User Last Name:" , _user_last_name)
+print("User Full Name:" , _user_full_name)
+
+_type_1 = type(_user_full_name)
+
+print("Type Of _user_full_name Var:" , _type_1)
+
+_if_cold = True
+_if_hot = False
+
+print("Cold:" , _if_cold)
+print("Hot" , _if_hot)
+
+_type_2  = type(_if_cold)
+
+print("Type Of _if_cold Var:" ,_type_2)
